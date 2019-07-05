16b32114afd36a924ce24c6849d0ee58dd3bebb9
Zaplatynskyi <hayk.ukr@gmail.com>
2019-07-05 16:58:03 +0300

    add changes according to comments

--- exercise_0/main.cpp
+++ exercise_0/main.cpp
@@ -4,77 +4,53 @@
 #include <vector>
 
 struct Result {
-    int vowels;
-    int consonant;
-    int others;
+    int vowels = 0;
+    int consonant = 0;
+    int others = 0;
 };
 
-Result countLetter(std::string firstLetter) {
-    Result result;
-    std::string vowels = "AEIOUYaeouy";
-    int countVowels = 0;
-    int countConsonant = 0;
-    int countOther = 0;
-    for (int i = 0; i < firstLetter.length(); i++) {
+const std::string vowels = "AEIOUYaeouy";
+
+Result countLetter(const std::string& firstLetter) {
+    Result result{};
+    for (int i = 0; i < firstLetter.length(); ++i) {
         if (isalpha(firstLetter[i]))
             if (vowels.find(firstLetter[i]) != std::string::npos)
-                countVowels += 1;
+                ++result.vowels;
             else
-                countConsonant += 1;
+                ++result.consonant;
         else
-            countOther += 1;
-    }
-    result.vowels = countVowels;
-    result.consonant = countConsonant;
-    result.others = countOther;
-    return result;
-}
-
-std::string firstLetterWord(std::string inputText) {
-    std::string result = "";
-    bool v = true;
-    for (int i = 0; i < inputText.length(); i++) {
-        if (inputText[i] == ' ')
-            v = true;
-        else if (inputText[i] != ' ' && v == true) {
-            result.push_back(inputText[i]);
-            v = false;
-        }
+            ++result.others;
     }
     return result;
 }
 
 
-int main() {
+int main(int argc, char **argv) {
     std::string line;
+    std::cout <<argv[0]<< std::endl;
+
     std::string firstLetters;
-    int args;
     Result res;
-    std::cout << "Enter 1 for read from file or 2  for read from console: " << std::endl;
-    std::cin >> args;
-    if (args == 1) {
-        std::ifstream file("file.txt");
+    if (argc == 2) {
+        std::ifstream file(argv[1]);
         if (file.is_open()) {
-            while (getline(file, line)) {
-                firstLetters.append(firstLetterWord(line));
+            while (file>>line) {
+                firstLetters+=line[0];
             }
             res = countLetter(firstLetters);
-            file.close();
         }
-    }else if (args == 2) {
+        else
+            perror("file not open");
+    }else if (argc == 1) {
         std::cout << "Enter words (q to quit):" << std::endl;
-        while (std::getline(std::cin, line)) {
-            if (line.find(" q") != std::string::npos) {
-                firstLetters.append(firstLetterWord(line.substr(0,line.find(" q"))));
-                std::cout <<firstLetters<< std::endl;
-                res = countLetter(firstLetters);
-                break;
-            } else
-                firstLetters.append(firstLetterWord(line));
+        while ((std::cin >> line)&&(line != "q")) {
+                firstLetters+=line[0];
         }
+        res = countLetter(firstLetters);
     }
     std::cout <<res.vowels <<" words beginning with vowels" << std::endl;
     std::cout <<res.consonant <<" words beginning with consonants " << std::endl;
     std::cout <<res.others <<" others" << std::endl;
-    return EXIT_FAILURE;
+    return 0;
 }
